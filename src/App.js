d12b2b5a925cea0f22c72d550e96a4aa8984ee8d
newsunbanjade <thenewsunbanjade@gmail.com>
2021-02-17 18:37:07 +0545

    final build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import logo from "./Logo.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="img">
+          <img src={logo} alt="" />
+          <h2>Enspire Tech</h2>
+        </div>
       </header>
+      <div className="center">
+        <h1>Coming Soon</h1>
+        <a href="https://facebook.com/enspiretech" target="_blank">
+          Contact Us
+        </a>
+      </div>
     </div>
   );
 }
